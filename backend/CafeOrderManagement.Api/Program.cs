4c97dede8874b80ec88eb9357c3422424ceb452e
bugrabildiren <bugrabildiren@gmail.com>
2026-04-19 22:54:13 +0300

    Prepare project structure and local Docker setup

--- backend/CafeOrderManagement.Api/Program.cs
+++ backend/CafeOrderManagement.Api/Program.cs
@@ -3,6 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDev",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -31,7 +42,7 @@ if (app.Environment.IsDevelopment())
 
 // app.UseHttpsRedirection();
 
-app.UseCors("AllowAngular");
+app.UseCors("AllowAngularDev");
 app.UseAuthorization();
 app.MapControllers();
 
